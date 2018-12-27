6daaad39dc9eb5c574df70d3283bb6c9570a4c57
Mopilo <mopahemma@gmail.com>
2018-12-28 00:12:28 +0100

    new components

---
+++ src/components/layouts/Index.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import Tracks from '../tracks/Tracks';
+
+
+
+export const Index = () => {
+    return(
+        <Fragment>
+            <Tracks/>
+        </Fragment>
+    )
+}
