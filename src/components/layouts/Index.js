5062895b9fa1914540b106cf7f89784766e20417
Mopilo <mopahemma@gmail.com>
2018-12-30 01:07:21 +0100

    added search component

--- src/components/layouts/Index.js
+++ src/components/layouts/Index.js
@@ -1,11 +1,13 @@
 import React, {Fragment} from 'react';
 import Tracks from '../tracks/Tracks';
+import Search from '../tracks/Search';
 
 
 
 export const Index = () => {
     return(
         <Fragment>
+            <Search/>
             <Tracks/>
         </Fragment>
     )
